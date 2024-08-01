82255730ba59c724f59d4a305b8f02aaaae4fa52
Nantha <nanthu1593@gmail.com>
2024-08-02 00:04:17 +0800

    Update QuestionController.java

--- src/main/java/com/learning/quiz_app/QuestionController.java
+++ src/main/java/com/learning/quiz_app/QuestionController.java
@@ -1,4 +1,15 @@
 package com.learning.quiz_app;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/question")
 public class QuestionController {
+    @GetMapping("allQuestions")
+    public String getAllQuestions(){
+        return "Hi , here's your all questions";
+    }
 }
