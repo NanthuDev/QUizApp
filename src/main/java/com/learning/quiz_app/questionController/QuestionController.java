98e9a9ce3249560fcc94bf6b01498275d861e07c
Nantha <nanthu1593@gmail.com>
2024-08-03 00:42:02 +0800

    Update QuestionController.java

--- src/main/java/com/learning/quiz_app/questionController/QuestionController.java
+++ src/main/java/com/learning/quiz_app/questionController/QuestionController.java
@@ -1,6 +1,8 @@
 package com.learning.quiz_app.questionController;
 
 
+import com.learning.quiz_app.questionService.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/question")
 public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
     @GetMapping("allQuestions")
     public String getAllQuestions(){
         return "Hi , here's your all questions";
