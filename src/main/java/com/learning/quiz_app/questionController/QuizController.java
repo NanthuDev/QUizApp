775c0cc56c43ef4ba3f5ee196e91bb6e3345a2f3
Nantha <nanthu1593@gmail.com>
2024-08-05 23:20:32 +0800

    Update QuizController.java

--- src/main/java/com/learning/quiz_app/questionController/QuizController.java
+++ src/main/java/com/learning/quiz_app/questionController/QuizController.java
@@ -1,4 +1,22 @@
 package com.learning.quiz_app.questionController;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("quiz")
 public class QuizController {
+
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam Integer noQ, @RequestParam String title){
+        return new ResponseEntity<>("Quiz", HttpStatus.OK);
+
+    }
+
+
 }
